771fed819398e5185a1498e8c8b3a747b2f58bf7
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-05-03 12:19:44 +0300

    feat: add storybook

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
+import StorybookUIRoot from './.storybook';
 
 // https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
+  
   const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
+  return <StorybookUIRoot> <ExpoRoot context={ctx} /> </StorybookUIRoot>;
 }
 
+export {default} from './.storybook';  
+
 registerRootComponent(App);
+
